4ad59bb28e31218a767766e6eb35e30d57c25c03
Ruixuan Dai <dairuixuan@wustl.edu>
2021-01-08 21:56:02 -0600

    Update 1.0
    1, changed the time format to Mongodb as a time object. (Previously, we use string for the time which is not suitable for index)
    2, Reorganized the code into two file
    3, Add 2-tier handler for writing error.
    4, Change the pymongo authentication with URI. URI can encode the username and password
    5, Add function to auto get RPi MAC

--- beacon_scan_log.py
+++ beacon_scan_log.py
@@ -1,17 +1,9 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
 from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter
 
 import logging
-import time
+import time, socket, fcntl, struct
 from datetime import datetime
-import os
-import random
-import threading
-import csv
-from logging.handlers import TimedRotatingFileHandler
-import pymongo
+import log_service
 
 #---------------------------------Configuration-------------------------------------
 send_interval_to_database = 10 # in seconds
@@ -21,127 +13,43 @@ interval_unit = 's'     # S - Seconds
                         # D - Days
                         # midnight - roll over at midnight
                         # W{0-6} - roll over on a certain day; 0 - Monday
-backupCount = 5
-total_scan_time_second = 30
+backupCount = 2
 
-
-mongo_db_ip_address = None # TODO: change this...
-
-#---------------------------------Connection-------------------------------------
-try:
-    assert mongo_db_ip_address is not None
-except:
-    raise Exception("\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\
-                    \nYou need to change the above 'mongo_db_ip_address' from None to the ip of the database.\
-                    \nYou can use ifconfig command in linux to find the ip address...\
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-myclient = pymongo.MongoClient(mongo_db_ip_address, 27017)
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
-
-# Some mac addresses 
-sample_token_mac_address = ["00:11:22:33:44:D{}".format(i) for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-sample_raspi_mac_address = ["09:FB:9B:F0:EF:D{}".format(i) for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-
-#---------------------------------Helper Functions-------------------------------------
-def _save_to_database(filename):
-    "Save a log in csv format to json and upload to the database"
-    with open(filename, newline='') as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=["time", "beacon_MAC", "pi_MAC", "uuid", "major", "minor", "RSSI", "tx_power"])
-        log_list = []
-        for row in reader:
-            log_list.append(row)
-        
-        # Option 1: using insert_many
-        # Cannot know which item is not inserted.
-        # x = mycol.insert_many(log_list)
-        # print(x)
-
-        # Option 2: Insert one-by-one
-        # TODO: handle the circumstance of loss of internet connection...
-        for _ent in log_list:
-            x = mycol.insert_one(_ent)
-            print(x)
-
-        # print(log_list)
-        os.remove(filename)
-
-
-class myTimeRotateFileHandler(TimedRotatingFileHandler):
-    def rotate(self, source, dest):
-        """
-        When rotating, rotate the current log.
-
-        The default implementation calls the 'rotator' attribute of the
-        handler, if it's callable, passing the source and dest arguments to
-        it. If the attribute isn't callable (the default is None), the source
-        is simply renamed to the destination.
-
-        :param source: The source filename. This is normally the base
-                       filename, e.g. 'test.log'
-        :param dest:   The destination filename. This is normally
-                       what the source is rotated to, e.g. 'test.log.1'.
-        """
-        if not callable(self.rotator):
-            # Issue 18940: A file may not have been created if delay is True.
-            if os.path.exists(source):
-                os.rename(source, dest)
-                print("New saved log file: {}".format(dest))
-                sendFunc = threading.Thread(target=_save_to_database, args=(dest,))
-                sendFunc.start()
-        else:
-            self.rotator(source, dest)
-
-    def getFilesToDelete(self):
-        return []
-
-#---------------------------------main program-------------------------------------
-# Create logger
-time_log = []
-rssi_log = []
-
-log_file = "timed_test.log"
-logger = logging.getLogger("Rotating Log")
-logger.setLevel(logging.INFO)
-
-handler = myTimeRotateFileHandler(log_file,
-                                    when=interval_unit,
-                                    interval=send_interval_to_database,
-                                    backupCount=backupCount)
-logger.addHandler(handler)
+log_file = "logs/timed_test.log"
+rpi_mac = None
 
 def callback(bt_addr, rssi, packet, additional_info):
     timestamp = datetime.now()
-    time_str = datetime.now().strftime("%Y-%m-%d-%H:%M:%S.%f")
-    # print("<%s, %d, %s>\n  %s\n %s\n" % (bt_addr, rssi, time_str, packet, additional_info))
     print( timestamp.isoformat(), bt_addr, rssi, packet.tx_power, packet.major, packet.minor, additional_info)
 
-    logger.info("{},{},{},{},{},{},{},{}".format(datetime.now(), bt_addr, sample_raspi_mac_address[0], 0, packet.major, packet.minor, rssi, packet.tx_power)) 
-    #"time", "beacon_MAC", "pi_MAC", "uuid", "major", "minor", "RSSI", "tx_power"
-    time_log.append(time.time())
-    rssi_log.append(rssi)
+    logger.info("{},{},{},{},{},{},{},{}".format(timestamp.isoformat(), bt_addr, rpi_mac, 0, packet.major, packet.minor, rssi, packet.tx_power)) 
 
-# scan for all TLM frames of beacons in the namespace "12345678901234678901"
-scanner = BeaconScanner(callback,
-    # remove the following line to see packets from all beacons
-    device_filter=None,
-    packet_filter=None
-)
-scanner.start()
-print("Start to scan for {} secnods".format(total_scan_time_second))
-time.sleep(total_scan_time_second)
-scanner.stop()
+def getHwAddr(ifname = 'wlan0'):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
+    return ':'.join('%02x' % b for b in info[18:24])
 
+if __name__ == "__main__":
 
-#---------------------------------plots for debug-------------------------------------
-# print (rssi_log)
+    print("Get Raspberry Pi MAC address...")
+    rpi_mac = getHwAddr()
+    print(rpi_mac)
 
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
 
-# tt = np.array(time_log)
-# rr = np.array(rssi_log)
+    handler = log_service.myTimeRotateFileHandler(log_file,
+                                        when=interval_unit,
+                                        interval=send_interval_to_database,
+                                        backupCount=backupCount)
+    logger.addHandler(handler)
 
-# np.save("time", tt)
-# np.save("rssi", rr)
+    # scan for all TLM frames of beacons in the namespace "12345678901234678901"
+    scanner = BeaconScanner(callback,
+        # remove the following line to see packets from all beacons
+        device_filter=None,
+        packet_filter=None
+    )
+    scanner.start()
+    print("Start to scan for beacons...")
 
-# plt.plot(tt - tt[0], rr)
-# plt.show()
---
+++ log_service.py
@@ -0,0 +1,104 @@
+import os, threading
+import csv
+import pymongo
+from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
+
+
+mongo_db_uri = "mongodb://admin:2baVmUJFrBj2epVU@192.168.0.109:27017/" # TODO: change this...
+#---------------------------------Connection-------------------------------------
+try:
+    assert mongo_db_uri is not None
+except:
+    raise Exception("\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\
+                    \nYou need to change the above 'mongo_db_uri' from None to the ip of the database.\
+                    \nYou can use ifconfig command in linux to find the ip address...\
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+myclient = pymongo.MongoClient(mongo_db_uri)
+mydb = myclient["BBCT"] # db names
+mycol = mydb["beacons"] # collection names
+
+error_file1 = "logs/error_tier_1.log"
+error_file2 = "logs/error_tier_2.log"
+
+
+#---------------------------------Helper Functions-------------------------------------
+def _write_file_to_database(filename, error_file, append=False):
+    # First to write error data last time
+    "Save a log in csv format to json and upload to the database"
+    if append:
+        write_mode = 'a'
+    else:
+        write_mode = 'w'
+    error_list = []
+    # Read the file and write the database
+    with open(filename,  newline='') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=["time", "beacon_MAC", "pi_MAC", "uuid", "major", "minor", "RSSI", "tx_power"])
+        log_list = []
+        
+        for row in reader:
+            log_list.append(row)
+            
+        for _ent in log_list:
+            # _ent["_id"] = 1 # Used for test writing error
+            _ent["time"] = datetime.fromisoformat(_ent["time"])
+            try:
+                x = mycol.insert_one(_ent)
+            except Exception as e:
+                print("Writing errors to db:", _ent, e)
+                _ent["time"] = _ent["time"].isoformat()
+                error_list.append(_ent)
+        # Search 
+        os.remove(filename)
+    
+    print("Error list", error_list)
+    if len(error_list) != 0:
+        # Write the entries with saving error. 
+        with open(error_file, mode=write_mode, newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=error_list[0].keys())
+            writer.writerows(error_list)
+
+
+def _save_to_database(filename):
+    """
+    Write the history data to the database.
+    Two tier error files to save entries that fail to be saved to DB
+    """
+    # First write the last error files 
+    if os.path.exists(error_file1):
+        print("Rewrite the 1st tier error file")
+        _write_file_to_database(error_file1, error_file2, append=True)
+
+    # Then write the current logfile
+    print("Write the current log", filename)
+    _write_file_to_database(filename, error_file1)
+
+
+
+class myTimeRotateFileHandler(TimedRotatingFileHandler):
+    def rotate(self, source, dest):
+        """
+        When rotating, rotate the current log.
+
+        The default implementation calls the 'rotator' attribute of the
+        handler, if it's callable, passing the source and dest arguments to
+        it. If the attribute isn't callable (the default is None), the source
+        is simply renamed to the destination.
+
+        :param source: The source filename. This is normally the base
+                       filename, e.g. 'test.log'
+        :param dest:   The destination filename. This is normally
+                       what the source is rotated to, e.g. 'test.log.1'.
+        """
+        if not callable(self.rotator):
+            # Issue 18940: A file may not have been created if delay is True.
+            if os.path.exists(source):
+                os.rename(source, dest)
+                print("New saved log file: {}".format(dest))
+                sendFunc = threading.Thread(target=_save_to_database, args=(dest,))
+                sendFunc.start()
+        else:
+            self.rotator(source, dest)
+
+    def getFilesToDelete(self):
+        return []
